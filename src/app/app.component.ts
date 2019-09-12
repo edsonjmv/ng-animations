133c5c218817888fc91db09d4a55a7d98fca72eb
Edson Machado <edson.machado@soprasteria.com>
2019-09-12 16:55:46 +0200

    adding fade animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from '../animations/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation]
 })
 export class AppComponent {
   show: boolean = true;
